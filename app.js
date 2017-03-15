59372f50578d5636af49c51f0597d8edad6e2092
alaamshabnan <alaamohamedshaban@yahoo.com>
2017-03-15 19:19:55 +0200

    module1-sol

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ angular.module('lunchcheck',[])
 .controller('LunchCheckController',LunchCheckController);
 LunchCheckController.$inject=['$scope'];
 function LunchCheckController($scope){
+  $scope.inputval = "";
   $scope.check=function(){
-     var array=$scope.inputval.split(',');
-  if(array.length>0 && array.length<=3){
+  var array=$scope.inputval.split(',');
+  if($scope.inputval==0){
+   $scope.notmatched=false;
+   $scope.matched=false;
+   $scope.noelem=true;
+   console.log(array.length);
+    console.log($scope.inputval.length);
+ }
+  else if(array.length>0 && array.length<=3){
     $scope.matched=true;
    $scope.notmatched=false;
+   $scope.noelem=false;
+   console.log(array.length);
+    console.log($scope.inputval.length);
   }
-  else {
+  else if(array.length>3) {
     $scope.notmatched=true;
     $scope.matched=false;
+    $scope.noelem=false;
   }
   }
   $scope.sayenjoy=function(){
@@ -21,7 +33,9 @@ function LunchCheckController($scope){
   $scope.saytoomuch=function(){
     return "Too much!"
   }
-
+  $scope.please=function(){
+    return "please"
+  }
 }
 
 
